c0e962edefd8ae0039ae9e44e28ca7f5d5eb9335
ralepromotions <ralepromotions@gmail.com>
2023-10-17 20:57:30 -0300

    Eliminacion de carpeta

--- JuegosFlix/js/script.js
+++
@@ -1,5 +0,0 @@
-function prevButton() { window.location.href = './index.html'; }
-function nextButton() { window.location.href = './index.html'; }
-function homeButton() { window.location.href = './index.html'; }
-function accionButton() { window.location.href = './accion.html'; }
-function subirButton() { window.scrollTo({top: 0}) }
